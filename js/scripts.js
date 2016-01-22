47f1d6ea4c2d0f0f8842fdd1425c3f43478d9d3c
Blake Scott <blakerscott@gmail.com>
2016-01-22 13:38:29 -0800

    add second spec test

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
+
+// var countTo = document.getElementById("pingpong");
+var countTo = 10
+
 var countUp = function() {
-    // var z = document.getElementById("pingpong");
-    var countTo = 10
-    var numberBank =[];
+    var numberBank = [];
+
     for (var i = 1; i <= countTo; i++) {
-      numberBank.push(i);
+      var ping = "PING"
+      // if (i % 3 === 0) {
+		  //     numberBank.push(ping);
+		  //     }
+      // else {
+      //     numberBank.push(i);
+      // }
     }
   return numberBank;
 };
+
+
+
+
+
 
 $(document).ready(function(){
   $("form").submit(function(event){
--- spec/specs.js
+++ spec/specs.js
@@ -4,4 +4,8 @@ describe('pingPong', function() {
   it("to count from one to the number specified", function() {
     expect(countUp()).to.eql([1,2,3,4,5,6,7,8,9,10]);
   });
+
+  it("will replace multiples of 3 with the word PING", function() {
+    expect(countUp()).to.eql([1,2,"PING",4,5,"PING",7,8,"PING",10]);
+  });
 });
